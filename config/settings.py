d04209c845b3b0e3ed2747781e6443ce8cb5f7c8
Leshane28 <leshane.jackson28@gmail.com>
2026-05-25 20:53:54 +0700

    Fix STATIC_ROOT for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for config project.
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 import os
 import dj_database_url
