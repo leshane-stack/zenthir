37ea1d171f0bdf5fa91ba57d5335cc0c8c29888c
Leshane28 <leshane.jackson28@gmail.com>
2026-06-05 13:30:24 +0700

    Serve sitemap index at /sitemap.xml with 200

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from django.views.static import serve
+from django.conf import settings
 from healthcare.views import robots_txt
+import os
+
+SITEMAP_DIR = os.path.join(settings.BASE_DIR, 'static_src', 'sitemaps')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', robots_txt, name='robots_txt'),
-    path('sitemap.xml', RedirectView.as_view(url='/static/sitemaps/sitemap.xml', permanent=True)),
+    path('sitemap.xml', serve, {'document_root': SITEMAP_DIR, 'path': 'sitemap.xml'}),
     path('', include('healthcare.urls')),
 ]
