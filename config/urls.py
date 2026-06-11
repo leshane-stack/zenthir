76fc6ed49dea6b76b3300574e5e32c4904232329
Leshane28 <leshane.jackson28@gmail.com>
2026-06-11 14:32:51 +0700

    Cache sitemaps for 24 hours

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap, index as sitemap_index
+from django.views.decorators.cache import cache_page
 from healthcare.views import robots_txt
 from healthcare.sitemaps import ProviderSitemap, ProcedureSitemap, StaticSitemap, CitySitemap, MarketPageSitemap
 
@@ -15,7 +16,7 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', robots_txt, name='robots_txt'),
-    path('sitemap.xml', sitemap_index, {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
-    path('sitemap-<section>.xml', sitemap, {'sitemaps': sitemaps}, name='sitemaps'),
+    path('sitemap.xml', cache_page(86400)(sitemap_index), {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
+    path('sitemap-<section>.xml', cache_page(86400)(sitemap), {'sitemaps': sitemaps}, name='sitemaps'),
     path('', include('healthcare.urls')),
 ]
