5b7d294cb7a3f0b18c85184620c29be5cce39f97
Leshane28 <leshane.jackson28@gmail.com>
2026-06-11 14:39:33 +0700

    Static sitemaps: filtered providers with pricing, procedures 5+ providers, market pages 10+ providers

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap, index as sitemap_index
-from django.views.decorators.cache import cache_page
+from django.views.static import serve
+from django.conf import settings
 from healthcare.views import robots_txt
-from healthcare.sitemaps import ProviderSitemap, ProcedureSitemap, StaticSitemap, CitySitemap, MarketPageSitemap
+import os
 
-sitemaps = {
-    'providers': ProviderSitemap,
-    'procedures': ProcedureSitemap,
-    'static': StaticSitemap,
-    'locations': CitySitemap,
-    'market': MarketPageSitemap,
-}
+SITEMAP_DIR = os.path.join(settings.BASE_DIR, 'static_src', 'sitemaps')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', robots_txt, name='robots_txt'),
-    path('sitemap.xml', cache_page(86400)(sitemap_index), {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
-    path('sitemap-<section>.xml', cache_page(86400)(sitemap), {'sitemaps': sitemaps}, name='sitemaps'),
+    path('sitemap.xml', serve, {'document_root': SITEMAP_DIR, 'path': 'sitemap.xml'}),
     path('', include('healthcare.urls')),
 ]
